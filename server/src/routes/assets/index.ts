0a696a2241ad7a1eb9063bc98fc7b2469bc48ebf
BurakYs <brkyhs0@gmail.com>
2024-06-01 17:21:21 +0300

    Videos are one-time links and deleted every restart

--- server/src/routes/assets/index.ts
+++ server/src/routes/assets/index.ts
@@ -16,12 +16,14 @@ export default async (fastify: FastifyInstance) => {
         handler: async (request: Request, response: Response) => {
             const { platform, name } = request.params as TiktokAsset;
 
-            const file = await fs.readFile(`./public/${platform}/${name}`).catch(() => null);
+            const filePath = `./public/${platform}/${name}`;
+            const file = await fs.readFile(filePath).catch(() => null);
             if (!file) {
                 response.sendError('File not found', 404);
                 return;
             }
 
+            await fs.unlink(filePath).catch(() => null);
             const extension = name.slice(name.lastIndexOf('.'));
             response.header('Content-Type', getMimeType(extension));
             response.code(200).send(file);
--- server/src/utils/deleteAssets.ts
+++ server/src/utils/deleteAssets.ts
@@ -1,18 +1,15 @@
-import { Cron } from 'croner';
 import { glob } from 'glob';
 import fs from 'fs/promises';
 
 export default function deleteAssets() {
     const platforms = ['tiktok'];
 
-    Cron('0 */3 * * *', async () => {
-        for (const platform of platforms) {
-            const files = await glob(`./public/${platform}/*`);
-            for (const file of files) {
-                if (!file.endsWith('.gitkeep')) {
-                    await fs.unlink(file);
-                }
+    for (const platform of platforms) {
+        const files = glob.sync(`./public/${platform}/*`);
+        for (const file of files) {
+            if (!file.endsWith('.gitkeep')) {
+                fs.unlink(file).catch(() => null);
             }
         }
-    });
+    }
 }
