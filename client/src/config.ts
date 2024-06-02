dda164da424dfe39c0595440b0f2755d02c9b280
BurakYs <brkyhs0@gmail.com>
2024-06-03 00:11:47 +0300

    change api url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
 export default {
-    rootUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api.vidget.buraky.live',
+    rootUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://vidget-api.buraky.live',
     cdnUrl: 'https://cdn.buraky.live',
 }
