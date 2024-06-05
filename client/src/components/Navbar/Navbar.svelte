10eb2aa046d527065336d92c1c0090a49e01650c
BurakYs <brkyhs0@gmail.com>
2024-06-05 10:26:22 +0300

    Add logo to sidebar

--- client/src/components/Navbar/Navbar.svelte
+++ client/src/components/Navbar/Navbar.svelte
@@ -67,8 +67,12 @@
 <div id="sidebar"
      class={`fixed top-0 right-0 w-64 h-full bg-secondary transform transition-transform duration-200 ease-in-out ${isSidebarOpen ? 'translate-x-0' : 'translate-x-full'} `}>
     <div class="flex flex-col h-full">
-        <div class="flex justify-end p-4">
-            <button on:click={closeSidebar} class="text-white" aria-label="Close Sidebar">
+        <div class="flex justify-between">
+            <div class="flex items-center px-4 py-6">
+                <img src="/icon.png" alt="Logo" width="32" height="32" class="mr-2">
+                <div class="text-3xl font-semibold">VidGet</div>
+            </div>
+            <button on:click={closeSidebar} class="text-white p-4" aria-label="Close Sidebar">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
                      class="h-6 w-6">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
