0810c1cd9d988c97d7a39fa45a005b9bb30c1668
BurakYs <brkyhs0@gmail.com>
2024-06-10 13:49:14 +0300

    Add cors back

---
+++ server/src/plugins/cors.ts
@@ -0,0 +1,10 @@
+import fp from 'fastify-plugin';
+import fastifyCors, { FastifyCorsOptions } from '@fastify/cors';
+
+const cors = fp(async (fastify) => {
+    fastify.register(fastifyCors, {
+        origin: '*'
+    } as FastifyCorsOptions);
+});
+
+export default cors;
