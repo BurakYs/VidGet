401cf18e4b08fed0240f0f77468ba424a43eff18
BurakYs <brkyhs0@gmail.com>
2024-07-11 12:43:54 +0300

    Translate error page

--- client/src/lib/i18n.ts
+++ client/src/lib/i18n.ts
@@ -1,6 +1,4 @@
 import { createI18n } from '@inlang/paraglide-sveltekit';
 import * as runtime from '$lib/paraglide/runtime.js';
 
-export const i18n = createI18n(runtime, {
-    prefixDefaultLanguage: 'always'
-});
+export const i18n = createI18n(runtime);
--- client/src/routes/+error.svelte
+++ client/src/routes/+error.svelte
@@ -1,20 +1,30 @@
 <svelte:head>
-    <title>Error</title>
+    <title>{m.error_title()}</title>
 </svelte:head>
 
 <script lang="ts">
-    import { page } from '$app/stores';
+  import { onMount } from 'svelte';
+  import { languageTag } from '$lib/paraglide/runtime.js';
+  import { page } from '$app/stores';
+  import { goto } from '$app/navigation';
+  import * as m from '$lib/paraglide/messages.js';
 
-    const errors: Record<string, string> = {
-        '4': 'Page not found',
-        '5': 'An error occurred on our side'
-    };
+  const errors: Record<string, string> = {
+    '4': m.error_page_not_found(),
+    '5': m.error_server_error()
+  };
 
-    const error = errors[$page.status.toString()[0]];
+  const error = errors[$page.status.toString()[0]];
+
+  onMount(() => {
+    setTimeout(() => {
+      goto(`/${languageTag()}`);
+    }, 5000);
+  });
 </script>
 
 <div class="flex flex-col items-center justify-center flex-grow p-4 text-center text-primary-text">
-    <h1 class="text-5xl font-semibold mb-2">Error {$page.status}</h1>
-    <p class="mb-8 text-lg text-secondary-text">{error}</p>
-    <img src="https://cdn.buraky.live/cat.png" alt="cat">
+    <h1 class="text-5xl font-semibold mb-2">{m.error_status_code({ statusCode: $page.status })}</h1>
+    <p class="mb-2 text-lg text-secondary-text">{error}</p>
+    <p class="mb-8 text-lg text-secondary-text">{m.error_redirect_home()}</p>
 </div>
