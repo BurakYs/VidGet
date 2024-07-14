6c8d5a8dbd8968ce64ee77e27a487aeb483ce8d8
BurakYs <brkyhs0@gmail.com>
2024-07-14 16:08:18 +0300

    Change cors config

--- server/src/plugins/cors.ts
+++ server/src/plugins/cors.ts
@@ -1,9 +1,11 @@
 import fp from 'fastify-plugin';
 import fastifyCors, { type FastifyCorsOptions } from '@fastify/cors';
+import app from '@/config/app';
 
 const cors = fp(async (fastify) => {
   fastify.register(fastifyCors, {
-    origin: '*'
+    methods: ['GET', 'POST'],
+    origin: app.rootUrl
   } as FastifyCorsOptions);
 });
 
