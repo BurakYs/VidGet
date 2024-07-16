aa86752216d7b984d55bdebea1ec4968d5152ca8
BurakYs <brkyhs0@gmail.com>
2024-07-16 22:40:53 +0300

    Update dependencies

--- server/src/utils/deleteAssets.ts
+++ server/src/utils/deleteAssets.ts
@@ -1,15 +1,15 @@
 import { glob } from 'glob';
 import fs from 'fs/promises';
+import app from '@/config/app';
 
 export default function deleteAssets() {
-  const platforms = ['tiktok'];
+  const platforms = app.supportedPlatforms.map(x => x.name.toLowerCase());
 
   for (const platform of platforms) {
-    const files = glob.sync(`./public/${platform}/*`);
+    const files = glob.sync(`./public/${platform}/**/*`);
     for (const file of files) {
-      if (!file.endsWith('.gitkeep')) {
-        fs.unlink(file).catch(() => null);
-      }
+      if (file.endsWith('.gitkeep')) continue;
+      fs.unlink(file).catch(() => null);
     }
   }
 }
