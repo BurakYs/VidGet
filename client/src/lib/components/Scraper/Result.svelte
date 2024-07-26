833deef67b84b81e39f32de20a17b55d77d0d0c5
BurakYs <brkyhs0@gmail.com>
2024-07-26 20:23:37 +0300

    Set details to null on dialog close

--- client/src/lib/components/Scraper/Result.svelte
+++ client/src/lib/components/Scraper/Result.svelte
@@ -4,34 +4,36 @@
   import detailsStore from '$lib/stores/details';
 </script>
 
-<Dialog.Root open={!!$detailsStore}>
+<Dialog.Root open={!!$detailsStore} onOpenChange={() => detailsStore.set(null)}>
     <Dialog.Content class="p-0 max-w-96">
-        <Dialog.Header class="text-start px-3 pt-3">
-            <div class="flex items-center">
-                <img
-                        alt={$detailsStore.author.nickname}
-                        class="w-10 h-10 rounded-full mr-3"
-                        src={$detailsStore.author.avatar}
+        {#if $detailsStore}
+            <Dialog.Header class="text-start px-3 pt-3">
+                <div class="flex items-center">
+                    <img
+                            alt={$detailsStore.author.nickname}
+                            class="w-10 h-10 rounded-full mr-3"
+                            src={$detailsStore.author.avatar}
+                    />
+                    {#if $detailsStore.author.nickname}
+                        <div>
+                            <p class="text-left text-xl font-semibold">
+                                {$detailsStore.author.nickname.slice(0, 24)}
+                            </p>
+                            <p class="text-left text-sm text-muted-foreground">
+                                @{$detailsStore.author.username}
+                            </p>
+                        </div>
+                    {:else}
+                        <p class="text-left text-lg">{$detailsStore.author.username}</p>
+                    {/if}
+                </div>
+            </Dialog.Header>
+            <div class="flex items-center justify-center">
+                <MediaPagination
+                        mediaList={$detailsStore.post.assets}
+                        soundUrl={$detailsStore.audio?.download || null}
                 />
-                {#if $detailsStore.author.nickname}
-                    <div>
-                        <p class="text-left text-xl font-semibold">
-                            {$detailsStore.author.nickname.slice(0, 24)}
-                        </p>
-                        <p class="text-left text-sm text-muted-foreground">
-                            @{$detailsStore.author.username}
-                        </p>
-                    </div>
-                {:else}
-                    <p class="text-left text-lg">{$detailsStore.author.username}</p>
-                {/if}
             </div>
-        </Dialog.Header>
-        <div class="flex items-center justify-center">
-            <MediaPagination
-                    mediaList={$detailsStore.post.assets}
-                    soundUrl={$detailsStore.audio?.download || null}
-            />
-        </div>
+        {/if}
     </Dialog.Content>
 </Dialog.Root>
--- client/src/lib/stores/details.ts
+++ client/src/lib/stores/details.ts
@@ -1,6 +1,6 @@
 import { type Writable, writable } from 'svelte/store';
 import type { ScraperResult } from '$lib/types';
 
-const detailsStore: Writable<ScraperResult> = writable();
+const detailsStore: Writable<ScraperResult | null> = writable();
 
 export default detailsStore;
